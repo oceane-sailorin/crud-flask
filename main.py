b12891bdca49408c6427ba512d8f8842aeaceb79
oceane <oceane.sailorin@live.com>
2021-05-04 23:18:16 +0200

    add update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 from app import create_app, db,  sport
 from app.sport import Wintersport
 
@@ -18,6 +19,14 @@ def home():
     wintersports = Wintersport.query.all()
     return render_template("home.html", wintersports=wintersports)
 
+@app.route("/update", methods=["POST"])
+def update():
+    newsport = request.form.get("newsport")
+    currentsport = request.form.get("currentsport")
+    wintersport = Wintersport.query.filter_by(name=currentsport).first()
+    wintersport.name = newsport
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run()
